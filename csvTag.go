1ebb525bd3bac356bf859b32b833ba460eb578a8
AidanJHMurphy <AidanJHMurphy@Gmail.com>
2022-09-19 12:55:00 -0500

    update isValidDataType to support more data types

--- csvTag.go
+++ csvTag.go
@@ -40,7 +40,7 @@ type csvAttributes struct {
 
 func isValidDataType(i interface{}) bool {
 	switch i.(type) {
-	case string, int, int8, int16, int32, int64, float32, float64:
+	case string, bool, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, complex64, complex128:
 		return true
 	}
 	return false
